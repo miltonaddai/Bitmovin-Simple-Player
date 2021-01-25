dee1953e2c5946ada5b135d3c54f2f393d164252
fliikamedia <admin@fliika.com>
2021-01-25 16:37:36 -0500

    new push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Player, PlayerEvent} from 'bitmovin-player';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const config = {
+  key: '72fc96e3-318b-452f-91c7-bed54f199dd1', 
+};
+
+const player = new Player(document.getElementById('root'), config);
+
+class App extends Component {
+  
+  render() {
+    
+    player.load({
+      
+      hls: 'https://fliikamediaservice-usea.streaming.media.azure.net/10052118-66f9-4c8f-9304-5f1fec768438/dreambelt.ism/manifest(format=m3u8-cmaf)',
+    }).then(() => {
+      player.play();
+    });
+    
+    return( 
+    <div>
+        Bitmovin testing Legoo
     </div>
-  );
+    )
+
+
+
+  }
+
 }
+
+ 
+
 
 export default App;
