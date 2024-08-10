3b7b70f5c2cd3748b62947472f34bd19a1a9ebc2
Milton <milton@fliika.com>
2024-08-10 00:07:31 -0400

    removed commented code

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,15 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDssG6lm2B3qHrZEsRi8SN6gr45zZ1G2mo",
-//     authDomain: "sonorous-cacao-297018.firebaseapp.com",
-//     projectId: "sonorous-cacao-297018",
-//     storageBucket: "sonorous-cacao-297018.appspot.com",
-//     messagingSenderId: "334242374492",
-//     appId: "1:334242374492:web:2a1a3ddd89dfc9c5c35799",
-//     measurementId: "G-V7V9Q3JNZT"
-//   };
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: "sonorous-cacao-297018.firebaseapp.com",
+    projectId: "sonorous-cacao-297018",
+    storageBucket: "sonorous-cacao-297018.appspot.com",
+    appId: "1:334242374492:web:2a1a3ddd89dfc9c5c35799",
+  };
 
-//   firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 
 
   export default firebase;
