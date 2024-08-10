23b808a852b3ff59b973576f18c6c656a5822e25
Milton <milton@fliika.com>
2024-08-09 22:05:56 -0400

    sample app first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import BitmovinPlayer from './bitmovinPlayer'
 
 function App() {
+    const [movieData, setMovieData] = useState({ hls: ""})
+    const [openModal, setOpenModal] = useState(false);
+
+    function ShowModal(){
+      setOpenModal(true);
+    }
+
   return (
-   <div id='player-wrapper'>
-     <BitmovinPlayer/>
-   </div>
+    <>
+  <div className="App">
+   <button className="button" onClick ={() => ShowModal()}> Click To Modal </button>
+  </div>
+      <div id='player-wrapper' openModal={openModal} setOpenModal= {setOpenModal} >
+      <BitmovinPlayer playerSource={movieData}/>
+    </div>
+</>
   );
 }
-
 export default App;
--- src/bitmovinPlayer.js
+++ src/bitmovinPlayer.js
@@ -3,61 +3,146 @@ import { Player } from 'bitmovin-player';
 import { UIFactory } from 'bitmovin-player/bitmovinplayer-ui';
 import 'bitmovin-player/bitmovinplayer-ui.css';
 
+
 class BitmovinPlayer extends React.Component {
-
     state = {
-        player: null,
+      player: null,
     };
-
+  
     playerConfig = {
-        key: '72fc96e3-318b-452f-91c7-bed54f199dd1'
+      key: process.env.REACT_APP_BITMOVIN_PLAYER_KEY,
+      playback: {
+        autoplay: true,
+      }
     };
-
-    playerSource = {
-        hls: 'https://fliikamediaservice-usea.streaming.media.azure.net/d4ac996a-f73e-4878-941e-0ed55f0ff783/Wildlife 1080p.ism/manifest(format=m3u8-cmaf)',
-        poster: 'https://testfliika.azureedge.net/thumbnails/nature-animals.jpg'
-    };
-
+  
     constructor(props) {
-        super(props);
-        this.playerDiv = React.createRef();
+      super(props);
+      this.playerDiv = React.createRef();
     }
-
+  
     componentDidMount() {
-        this.setupPlayer();
-    }
-
-    componentWillUnmount() {
-        this.destroyPlayer();
-    }
-
-    setupPlayer() {
-        const player = new Player(this.playerDiv.current, this.playerConfig);
-        UIFactory.buildDefaultUI(player);
-        player.load(this.playerSource).then(() => {
-            this.setState({
-                ...this.state,
-                player
-            });
-            console.log('Successfully loaded source');
-        }, () => {
-            console.log('Error while loading source');
-        });
-    }
-
-    destroyPlayer() {
-        if (this.state.player != null) {
-            this.state.player.destroy();
-            this.setState({
-                ...this.state,
-                player: null
-            });
+        if(this.props.playerSource!==null){
+          this.setupPlayer();
         }
     }
-
-    render() {
-        return <div id='player' ref={this.playerDiv}/>;
+  
+    componentDidUpdate(prevProps) {
+      if (this.props.playerSource!==null || (this.props.playerSource.hls !== prevProps.playerSource.hls)) {
+        this.changePlayerSource();
+      }
     }
-}
+  
+    componentWillUnmount() {
+      this.destroyPlayer();
+    }
+  
+    setupPlayer() {
+      const player = new Player(this.playerDiv.current, this.playerConfig);
+      UIFactory.buildDefaultUI(player);
+      player.load(this.props.playerSource).then(
+        () => {
+          this.setState({
+            ...this.state,
+            player,
+          });
+          console.log("Successfully loaded source");
+        },
+        () => {
+          console.log("Error while loading source");
+        }
+      );
+    }
+  
+    changePlayerSource() {
+      if (this.state.player == null) {
+          this.setupPlayer();
+      } else {
+        this.state.player.load(this.props.playerSource).then(
+          () => {
+            console.log("Successfully new loaded source");
+          },
+          () => {
+            console.log("Error while loading source");
+          }
+        );
+      }
+    }
+  
+    destroyPlayer() {
+      if (this.state.player != null) {
+        this.state.player.destroy();
+        this.setState({
+          ...this.state,
+          player: null,
+        });
+      }
+    }
+  
+    render() {
+      return <div id="player" ref={this.playerDiv} />;
+    }
+  }
+  
+  export default BitmovinPlayer;
 
-export default BitmovinPlayer;
+
+
+// class BitmovinPlayer extends React.Component {
+
+//     state = {
+//         player: null,
+//     };
+
+//     playerConfig = {
+//         key: '72fc96e3-318b-452f-91c7-bed54f199dd1'
+//     };
+
+//     // playerSource = {
+//     //     hls: '',
+//     //     poster: ''
+//     // };
+
+//     constructor(props) {
+//         super(props);
+//         this.playerDiv = React.createRef();
+//     }
+
+//     componentDidMount() {
+//         this.setupPlayer();
+//     }
+
+//     componentWillUnmount() {
+//         this.destroyPlayer();
+//     }
+
+//     setupPlayer() {
+//         const player = new Player(this.playerDiv.current, this.playerConfig);
+//         UIFactory.buildDefaultUI(player);
+//         player.load(this.playerSource).then(() => {
+//             this.setState({
+//                 ...this.state,
+//                 player
+//             });
+//             console.log('Successfully loaded source');
+//         }, () => {
+//             console.log('Error while loading source');
+//         });
+//     }
+
+//     destroyPlayer() {
+//         if (this.state.player != null) {
+//             this.state.player.destroy();
+//             this.setState({
+//                 ...this.state,
+//                 player: null
+//             });
+//         }
+//     }
+
+//     render() {
+//         return <div id='player' ref={this.playerDiv}/>;
+//     }
+// }
+
+// export default BitmovinPlayer;
